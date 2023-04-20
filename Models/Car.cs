45186e31719a2ac15feb67520189e2409f2568da
Sofia Diaz Baron <sofiadiazbaron@gmail.com>
2023-04-20 10:59:59 -0300

    Initial commit

---
+++ Models/Car.cs
@@ -0,0 +1,11 @@
+namespace Clase5.Models;
+
+public class Car{
+    public int Id { get; set; }
+    public string Modelo { get; set; }
+    public int Año { get; set; }
+    public string Marca { get; set; }
+    public string Patente { get; set; }
+    public string Color { get; set; }
+}
+
